94f16136074f42f283128c55785466477dfb8aa4
Dedan Irungu <dedanirungu@gmail.com>
2022-09-03 12:47:39 +0300

    changes to base

---
+++ menus.php
@@ -0,0 +1,3 @@
+<?php
+
+$this->add_submenu("account", "setting", "Settings", "/base/admin/settings/manage", 1);
---
+++ views/admin/settings/manage.vue
@@ -0,0 +1,43 @@
+<template>
+    <table-edit :path_param="path_param" :model="model">
+        <div class="row">
+            <div class="col-md-6">
+                <FormKit label="Id" id="id" type="hidden" validation="required" />
+                <FormKit label="Title" id="title" type="text" validation="required" />
+                <FormKit label="Slug" id="slug" type="text" validation="required" />
+                <FormKit label="Value" id="value" type="text" validation="required" />
+                <FormKit label="Start Amount" id="start_amount" type="text" validation="required" />
+                <FormKit label="End Amount" id="end_amount" type="text" validation="required" />
+            </div>
+            <div class="col-md-6">
+                <FormKit label="Is Percent" id="is_percent" type="text" validation="required" />
+                <FormKit label="Is Visible" id="is_visible" type="text" validation="required" />
+                <FormKit label="Published" id="published" type="text" validation="required" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <FormKit label="File Limit" id="file_limit" type="text" validation="required" />
+                <FormKit label="File Type" id="file_type" type="text" validation="required" />
+                <FormKit label="File Structure" id="file_structure" type="text" validation="required" />
+                <FormKit label="File Suffix" id="file_suffix" type="text" validation="required" />
+            </div>
+        </div>
+
+    </table-edit>
+</template>
+
+<script>
+export default {
+
+    updated () {
+
+    },
+
+    methods: {
+        getNow: function () {
+
+        }
+    }
+};
+</script>
