917210d31921d86909ae09d37a08dae8b800c13d
Dedan Irungu <dedanirungu@gmail.com>
2022-09-06 11:37:57 +0300

    changes to setting

--- Classes/FetchMenus.php
+++ Classes/FetchMenus.php
@@ -11,9 +11,7 @@ class FetchMenus
 
     public function fetchMenus()
     {
-
         $DS = DIRECTORY_SEPARATOR;
-
 
         $modules_path = realpath(base_path()) . $DS . 'Modules';
 
@@ -28,15 +26,47 @@ class FetchMenus
 
                     foreach ($file_names as $key => $file_name) {
                         $menu_file = $modules_path .  $DS . $module_name .  $DS . $file_name . '.php';
+
                         if (file_exists($menu_file)) {
                             include_once $menu_file;
                         }
                     }
-
-
-
                 }
             }
+        }
+
+        // Reorder Menu
+        foreach ($this->menus as $module => $menu) {
+            $column = 'position';
+            $tmp_menus = $this->menus[$module]['menus'];
+
+            usort($tmp_menus, function ($a, $b) use ($column) {
+                if (isset($a[$column]) && isset($b[$column])) {
+                    return $a[$column] <=> $b[$column];
+                }
+                return -1;
+            });
+
+
+            foreach ($tmp_menus as $key => $tmp_submenu) {
+                $tmp_submenu_list = $tmp_submenu['list'];
+
+                unset($tmp_menus[$key]);
+                $tmp_menus[$tmp_submenu['key']] = $tmp_submenu;
+
+                usort($tmp_submenu_list, function ($a, $b) use ($column) {
+                    if (isset($a[$column]) && isset($b[$column])) {
+                        return $a[$column] <=> $b[$column];
+                    }
+                    return -1;
+                });
+
+                $tmp_menus[$tmp_submenu['key']]['list'] = [];
+                $tmp_menus[$tmp_submenu['key']]['list'] =  $tmp_submenu_list;
+            }
+
+            $this->menus[$module]['menus'] = [];
+            $this->menus[$module]['menus'] = $tmp_menus;
         }
 
         return $this->menus;
@@ -49,24 +79,30 @@ class FetchMenus
             $this->menus[$module] = ['menus' => []];
         }
 
+        $data['key'] = $module;
+
         $this->menus[$module] = array_merge($this->menus[$module], $data);
     }
 
     public function add_menu($module, $key, $title, $path, $icon, $position)
     {
-        $this->menus[$module]['menus'][$key] = [
-            'title' => $title,
-            'path' => $path,
-            'position' => $position,
-            'icon' => $icon,
-            'list' => []
-        ];
+        $this->menus[$module]['menus'][$key]['title'] = $title;
+        $this->menus[$module]['menus'][$key]['key'] = $key;
+        $this->menus[$module]['menus'][$key]['path'] = $path;
+        $this->menus[$module]['menus'][$key]['position'] = $position;
+        $this->menus[$module]['menus'][$key]['icon'] = $icon;
+
+
+        if (!isset($this->menus[$module]['menus'][$key]['list'])) {
+            $this->menus[$module]['menus'][$key]['list'] = [];
+        }
     }
 
     public function add_submenu($module, $key, $title, $path, $position)
     {
         $this->menus[$module]['menus'][$key]['list'][] = [
             'title' => $title,
+            'key' => $key,
             'path' => $path,
             'position' => $position,
         ];
