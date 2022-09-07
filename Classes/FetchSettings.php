c6590ae852edc512228df0358d7e0d22fec7cfc1
Dedan Irungu <dedanirungu@gmail.com>
2022-09-07 14:21:28 +0300

    changes to  setting

--- Classes/FetchSettings.php
+++ Classes/FetchSettings.php
@@ -17,19 +17,43 @@ class FetchSettings
 
         $modules_path = realpath(base_path()) . $DS . 'Modules';
 
+
+
         if (is_dir($modules_path)) {
             $dir = new \DirectoryIterator($modules_path);
 
             foreach ($dir as $fileinfo) {
                 if (!$fileinfo->isDot() && $fileinfo->isDir()) {
                     $module_name = $fileinfo->getFilename();
+                    $module_name_slug = Str::lower($module_name);
 
                     $file_names = ['setting', 'settings'];
 
                     foreach ($file_names as $key => $file_name) {
                         $setting_file = $modules_path .  $DS . $module_name .  $DS . $file_name . '.php';
                         if (file_exists($setting_file)) {
-                            include_once $setting_file;
+                            $this->add_module_info($module_name_slug, [
+                                'title' => $module_name,
+                                'description' => $module_name,
+                            ]);
+
+                            $settings = include_once $setting_file;
+
+                            foreach ($settings as $setting_name => $setting) {
+                                $category = (isset($setting['category']) && $setting['category'] != '')
+                                    ? $setting['category']
+                                    : 'Main';
+                                $category_slug = Str::snake(Str::lower($category));
+
+                                $category_position = ($category_slug == 'main') ? 5 : 1;
+                                $field_position = (isset($setting['position']) && $setting['position'] != '')
+                                    ? $setting['position']
+                                    : 5;
+
+                                $this->add_setting_category($module_name_slug, $category_slug, $category, $category_position);
+
+                                $this->add_setting($module_name_slug, $category_slug, $setting_name, $field_position, $setting);
+                            }
                         }
                     }
                 }
@@ -49,7 +73,7 @@ class FetchSettings
         $this->settings[$module] = array_merge($this->settings[$module], $data);
     }
 
-    public function add_setting_category($module, $key, $title, $position, $params)
+    public function add_setting_category($module, $key, $title, $position, $params = [])
     {
         $this->settings[$module]['settings'][$key] = [
             'title' => $title,
@@ -59,10 +83,10 @@ class FetchSettings
         ];
     }
 
-    public function add_setting($module, $key, $title, $position, $params)
+    public function add_setting($module, $key, $name, $position, $params)
     {
         $this->settings[$module]['settings'][$key]['list'][] = [
-            'title' => $title,
+            'name' => $name,
             'position' => $position,
             'params' => $params
         ];
