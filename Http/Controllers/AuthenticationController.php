16d2fef4ae50a937dc9f9fda985b03548316b43a
Dedan Irungu <dedanirungu@gmail.com>
2023-01-07 16:41:46 +0300

    Adding Authentication to Base Module

--- Http/Controllers/AuthenticationController.php
+++ Http/Controllers/AuthenticationController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
+use App\Http\Controllers;
 
 class AuthenticationController extends Controller
 {
