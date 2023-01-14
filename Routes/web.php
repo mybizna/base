4fdc8f98203e70a8fd205baeed2d57d600589301
Dedan Irungu <dedanirungu@gmail.com>
2023-01-14 14:08:29 +0300

    adding clear artisan url

--- Routes/web.php
+++ Routes/web.php
@@ -16,5 +16,39 @@ use Modules\Base\Http\Controllers\BaseController;
 //manage new user
 Route::get('/manage', [BaseController::class, 'manage']);
 
+// Clear application cache:
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    return 'Application cache has been cleared';
+});
+
+//Clear route cache:
+Route::get('/route-cache', function() {
+	Artisan::call('route:cache');
+    return 'Routes cache has been cleared';
+});
+
+//Clear config cache:
+Route::get('/config-cache', function() {
+ 	Artisan::call('config:cache');
+ 	return 'Config cache has been cleared';
+}); 
+
+// Clear view cache:
+Route::get('/view-clear', function() {
+    Artisan::call('view:clear');
+    return 'View cache has been cleared';
+});
+
+Route::get('/reset-all', function() {
+
+    Artisan::call('cache:clear');
+    Artisan::call('route:cache');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+
+    return 'Clear all reset.';
+});
+
 require_once 'auth.php';
 
