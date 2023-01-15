68454f2c5dee31bbbce8be958af63a0daa480f55
Dedan Irungu <dedanirungu@gmail.com>
2023-01-15 12:19:32 +0300

    Updates to logging page

--- Resources/views/app.blade.php
+++ Resources/views/app.blade.php
@@ -12,11 +12,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/css/intlTelInput.css"
         integrity="sha512-gxWow8Mo6q6pLa1XH/CcH8JyiSDEtiwJV78E+D+QP0EVasFs8wKXq16G8CLD4CJ2SnonHr4Lm/yY2fSI2+cbmw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="http://127.0.0.1:8000/mybizna/css/app.css?{{ rand(10000, 50000) }}"></script>
     <script src="http://127.0.0.1:8000/mybizna/js/tailwindcss.js?{{ rand(10000, 50000) }}"></script>
-
+    <link href="http://127.0.0.1:8000/mybizna/fontawesome/css/all.css" rel="stylesheet">
 </head>
 
-<body>
+<body class="bg-blue-50 dark:bg-blue-900">
 
     @yield('content')
 
