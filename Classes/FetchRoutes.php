7717a1c8d11c2892504392bca52c95db9cf9faa2
Dedan Irungu <dedanirungu@gmail.com>
2023-02-05 18:48:28 +0300

    adding search form to views and make form.vue default for create and edit

--- Classes/FetchRoutes.php
+++ Classes/FetchRoutes.php
@@ -3,6 +3,7 @@
 namespace Modules\Base\Classes;
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
 class FetchRoutes
@@ -51,7 +52,6 @@ class FetchRoutes
 
                 $this->routes = array_merge($this->routes, $routes);
 
-
                 Cache::put("fetch_layouts", $this->layouts, 3600);
                 Cache::put("fetch_routes", $this->routes, 3600);
             }
@@ -91,25 +91,46 @@ class FetchRoutes
 
                             foreach ($vs_dir as $vs_fileinfo) {
                                 if (!$vs_fileinfo->isDot() && !$vs_fileinfo->isDir()) {
+                                    $search_path = '';
                                     $vs_filename = $vs_fileinfo->getFilename();
                                     $vs_sx_filename = str_replace('.vue', '', $vs_filename);
                                     $vs_path = $module_name . '/' . $folder . '/' . $vs_foldername . '/' . $vs_sx_filename;
 
                                     $t_folder_path = $v_folder_path . '/' . $vs_sx_filename;
 
+                                    if (File::isFile($vs_folders . '/search.vue')) {
+                                        $search_path = $t_folder_path . '/search.vue';
+                                    }
+                                    
                                     if ($vs_sx_filename == 'list') {
-                                        $vs_route['children'][] = $this->addRouteToList($vs_sx_filename, $t_folder_path, $vs_path . '.vue', true);
-                                        $vs_route['children'][] = $this->addRouteToList($vs_sx_filename, $t_folder_path, $vs_path . '.vue');
+                                        $vs_route['children'][] = $this->addRouteToList($vs_sx_filename, $t_folder_path, $vs_path . '.vue', true, search_path:$search_path);
+                                        $vs_route['children'][] = $this->addRouteToList($vs_sx_filename, $t_folder_path, $vs_path . '.vue', search_path:$search_path);
+                                    } else if ($vs_sx_filename == 'search') {
+                                        continue;
+                                    } else if ($vs_sx_filename == 'form') {
+                                        if (!File::isFile($vs_folders . '/create.vue')) {
+                                            $vs_route['children'][] = $this->addRouteToList('create', $v_folder_path . '/create', $vs_path . '.vue');
+                                        }
+
+                                        if (!File::isFile($vs_folders . '/edit.vue')) {
+                                            $vs_route['children'][] = $this->addRouteToList('edit/:id', $v_folder_path . '/edit', $vs_path . '.vue');
+                                        }
                                     } else {
                                         if (in_array($vs_filename, ['edit.vue', 'modify.vue', 'detail.vue', 'update.vue'])) {
                                             $vs_sx_filename = $vs_sx_filename . '/:id';
                                         }
-                                        $vs_route['children'][] = $this->addRouteToList($vs_sx_filename, $t_folder_path, $vs_path . '.vue');
+
+                                        if (in_array($vs_filename, ['create.vue', 'edit.vue', 'modify.vue', 'new.vue', 'detail.vue', 'update.vue', 'form.vue'])) {
+                                            $search_path = '';
+                                        }
+
+                                        $vs_route['children'][] = $this->addRouteToList($vs_sx_filename, $t_folder_path, $vs_path . '.vue', search_path:$search_path);
                                     }
 
                                     if (!in_array($vs_filename, ['create.vue', 'edit.vue', 'modify.vue', 'new.vue', 'detail.vue', 'update.vue', 'form.vue'])) {
                                         $this->layouts[$module_name][$folder][$vs_foldername][] = $vs_filename;
                                     }
+
                                 }
                             }
                         }
@@ -133,11 +154,18 @@ class FetchRoutes
         return $routes;
     }
 
-    public function addRouteToList($path, $name, $component, $no_path = false)
+    public function addRouteToList($path, $name, $component, $no_path = false, $search_path = '')
     {
+
+        $meta = ['breadcrumb' => true, 'middlewareAuth' => true];
+
+        if ($search_path != '') {
+            $meta = ['breadcrumb' => true, 'middlewareAuth' => true, 'search_path' => Str::lower($search_path)];
+        }
+
         return [
             'path' => $no_path ? '' : Str::lower($path),
-            'meta' => ['breadcrumb' => true, 'middlewareAuth' => true],
+            'meta' => $meta,
             'name' => $no_path ? Str::lower(str_replace('/', '.', $name)) . '.default' : Str::lower(str_replace('/', '.', $name)),
             'component' => Str::lower($component),
         ];
