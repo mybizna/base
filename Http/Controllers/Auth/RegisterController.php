fdf7d32748e8c8ce6efbe8e6b184bb9d06281a92
Dedan Irungu <dedanirungu@gmail.com>
2023-03-29 08:44:19 +0300

    Fixing registration by adding instruction for adding username and phone

--- Http/Controllers/Auth/RegisterController.php
+++ Http/Controllers/Auth/RegisterController.php
@@ -82,6 +82,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            'username' => $data['username'],
+            'phone' => $data['phone'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
