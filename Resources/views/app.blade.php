9e0b0c86650aed9f3283305f180607440c9e71f1
Dedan <dedanirungu@gmail.com>
2023-11-14 19:29:07 +0300

    Improvent to security policy

--- Resources/views/app.blade.php
+++ Resources/views/app.blade.php
@@ -7,8 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    @if($force_https)
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-
+    @endif
+    
     <title>ISP Access</title>
 
 
--- Resources/views/front.blade.php
+++ Resources/views/front.blade.php
@@ -4,7 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    @if($force_https)
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    @endif
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
