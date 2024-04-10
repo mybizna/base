7e18cbcd6892be70b9ce9f320dc5dc0cd98cabf5
Dedan <dedanirungu@gmail.com>
2024-04-10 18:27:05 +0300

    Fix formatting issues in Register.vue

--- Resources/vue/guest/Register.vue
+++ Resources/vue/guest/Register.vue
@@ -147,13 +147,4 @@ export default {
   font-size: 12px !important;
 }
 
-#login {
-  height: 50%;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  content: "";
-  z-index: 0;
-}
 </style>
