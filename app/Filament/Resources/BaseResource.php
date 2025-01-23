dfbc8d1e5f93d17fe6970072361cafd38135ef40
Dedan <dedanirungu@gmail.com>
2025-01-23 03:43:06 +0300

    Make Filament Resource Shorter

--- app/Filament/Resources/BaseResource.php
+++ app/Filament/Resources/BaseResource.php
@@ -5,34 +5,15 @@ namespace Modules\Base\Filament\Resources;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables\Table;
+use Miguilim\FilamentAutoPanel\AutoResource;
 
-class BaseResource extends Resource
+
+class BaseResource extends AutoResource
 {
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     protected static ?int $navigationSort = 1;
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([])
-            ->filters([])
-            ->actions([])
-            ->bulkActions([]);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
 
 }
